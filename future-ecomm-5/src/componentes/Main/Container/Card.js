c5414517b0a95a2887686b73d750080a9c6a54b3
Pedro Severo <phsevero94@gmail.com>
2019-10-10 14:21:08 -0300

    Criação do Container com cards

---
+++ future-ecomm-5/src/componentes/Main/Container/Card.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import styled from 'styled-components';
+
+export function Card () {
+    return (
+        <MainCard>
+            
+        </MainCard>
+    )
+}
---
+++ future-ecomm-5/src/componentes/Main/Container/CardContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Card } from './Card';
+
+const MainCardConteiner = styled.div `
+`
+
+export function CardContainer() {
+    return (
+        <MainCardConteiner>
+            
+        </MainCardConteiner>
+    )
+}
